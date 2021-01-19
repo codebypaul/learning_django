3cce4c54d01b4154e9ca81a0112ca36bc510ec2b
Paul Williams <paul.d.williamsxiv@gmail.com>
2021-01-19 10:57:08 -0500

    blog application can now take in user profile photos, as well as change user information and create new posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView
 from . import views
 
 urlpatterns = [
-    path('',views.home, name='blog_home'),
-    path('blog/', views.blog, name='actually_blog'),
-    path('about/', views.about, name='blog_about')
+    path("", views.home, name="blog_home"),
+    path("blog/", PostListView.as_view(), name="actually_blog"),
+    path("post/<int:pk>", PostDetailView.as_view(), name="post-detail"),
+    path("post/new", PostCreateView.as_view(), name="post-create"),
+    path("about/", views.about, name="blog_about"),
 ]
