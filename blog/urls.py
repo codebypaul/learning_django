9b4934293028b4b1badf3d2c20906ee75bde047c
Paul Williams <paul.d.williamsxiv@gmail.com>
2021-01-19 12:46:34 -0500

    users can now creat, update, and delete posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
 from . import views
 
 urlpatterns = [
     path("", views.home, name="blog_home"),
     path("blog/", PostListView.as_view(), name="actually_blog"),
     path("post/<int:pk>", PostDetailView.as_view(), name="post-detail"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
     path("post/new", PostCreateView.as_view(), name="post-create"),
     path("about/", views.about, name="blog_about"),
 ]
